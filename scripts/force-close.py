99792b2c82b1e29675af1ae19cccf1b33d0583d4
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-09-30 19:50:51 -0300

    return status

--- scripts/force-close.py
+++ scripts/force-close.py
@@ -1,16 +1,23 @@
-import gradio as gr
 import os
+import asyncio
+import gradio as gr
+from fastapi import BackgroundTasks
+
+async def do_exit():
+    await asyncio.sleep(0.2)
+    os._exit(0)
 
 def on_ui_tabs():
     with gr.Blocks() as tab:
         close_button = gr.Button("Force close webui")
-        close_button.click(lambda: os._exit(0))
+        close_button.click(do_exit)
     return [(tab, "Force Close", "force_close")]
 
 def on_app_started(blocks, app):
     @app.post("/force_close")
-    async def force_close():
-        os._exit(0)
+    async def force_close(background_tasks: BackgroundTasks):
+        background_tasks.add_task(do_exit)
+        return {"message": "Shutting down..."}
 
 try:
     from modules import script_callbacks
